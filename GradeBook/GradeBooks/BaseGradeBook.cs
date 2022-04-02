f728594c2882ece102f6e41b789a955abfc043bd
Mateusz <mateusz.tobiasz@isprzet.pl>
2022-04-02 11:50:25 +0200

    Updated BaseGradeBook.GetGPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -108,20 +108,28 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var GPA = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    GPA = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    GPA = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    GPA = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    GPA = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    GPA = 0;
+                    break;
             }
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                GPA++;
+            return GPA;
         }
 
         public virtual void CalculateStatistics()
