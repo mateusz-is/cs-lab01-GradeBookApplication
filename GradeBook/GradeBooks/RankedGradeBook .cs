699377fdd5ad979ac9076d3cff2c80137a2633be
Mateusz <mateusz.tobiasz@isprzet.pl>
2022-04-02 11:02:55 +0200

    Overrided RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook .cs
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -32,6 +32,18 @@ namespace GradeBook.GradeBooks
                 return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
 
     }
 }
